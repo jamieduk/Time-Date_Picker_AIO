b0b6b3f39b1e8b3fdabfb58fa2ad09b47a321e48
Jay <jamied_uk@hotmail.com>
2019-06-22 17:40:14 +0100

    Update test.php

--- test.php
+++ test.php
@@ -41,13 +41,120 @@ ini_set("display_errors", "0");
 error_reporting(E_ALL);
 ?>
 
-        <h1>Set Email Alarm</h1>
+<h1>Set Email Alarm</h1>
 
 
-        <style type="text/css">
-            body {
-                font: 14px monospace;
-            }
+<style type="text/css">
+    body {
+font: 14px monospace;
+    }
+</style>
+<center>
+ <script type="text/javascript" src="js/IP_generalLib.js"></script>
+<h1>Select A Date</h1>
+<form action="Set_Future_Reminder.php" id="Time_And_Date"><center>
+<input type="text" name="date1" id="date1" alt="date" class="IP_calendar" title="d/m/Y"><p>
+ 
+<h1>Select A Time</h1>
+<input type="time" id="time" name="time" value="01:30" min="0:00" max="18:02"><center>
+
+<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/jquery.clockinput.css">
+<script type="text/javascript" src="js/jquery.clockinput.js"></script>
+<script type="text/javascript">
+    $("input[type=time]").clockInput(false);
+</script>
+
+
+<center>
+<h1>Choose A Description</h1>
+  <input type="text" id="description" name="description" placeholder="Reminder Description" >
+  <input type="submit" value="Set" style="cursor:pointer;" >
+</form>
+<!-- the result of the search will be rendered inside this div -->
+<div id="result3"></div> <div id="result"></div> <div id="result2"></div>
+<div id="msg"></div>
+<script>
+// Attach a submit handler to the form
+$( "#Time_And_Date" ).submit(function( event ) {
+ 
+  // Stop form from submitting normally
+  event.preventDefault();
+ 
+  // Get some values from elements on the page:
+  var $form=$( this ),
+    date=$form.find( "input[name='date1']" ).val(),
+    time=$form.find( "input[name='time']" ).val(),
+    description=$form.find( "input[name='description']" ).val(),
+    url=$form.attr( "action" );
+ 
+  // Send the data using post
+  var posting=$.post( url, { time: time } );
+  var posting=$.post( url, { date1: date } );
+  var posting=$.post( url, { description: description } );
+  // Put the results in a div
+  posting.done(function( data ) {
+   // var content=$( data ).find( "#date1" );
+    $( "#result2" ).empty().append( time ); //content
+    $( "#result" ).empty().append( date ); //content
+    $( "#result3" ).empty().append( description ); //content
+// Time & Date Not Set! check here!
+  });
+});
+</script>
+
+
+<?php
+
+if(isset($_POST['submit'])) {
+$persinfo=preg_replace('#[^a-z0-9 :._]#i', '', $_POST['persinfo']);
+$sql="UPDATE alarm_email SET details='$persinfo', date='$persinfo1' WHERE username ='$u'";
+$query=mysqli_query($db_conx, $sql);
+session_start();
+$_SESSION["Saved"]='success';
+echo "<center>Your Future Email Alarm Has Been Updated!<br />";
+header("location: user.php?u=".$_SESSION["username"]);
+} else {
+// Update failed!
+//echo "Failed To Update!";
+}
+
+$numrows='0';
+$res='';
+
+$sql="SELECT * FROM alarm_email WHERE username ='$u' LIMIT 1";
+$user_query=mysqli_query($db_conx, $sql);
+mysqli_store_result($db_conx);
+$numrows=mysqli_num_rows($user_query);
+if($numrows < 1) {
+
+$res   ='<p> ';
+$res  .= 'No Reminders Set For Future Reminder Emails!';
+} else {
+while($row=mysqli_fetch_array($user_query, MYSQLI_ASSOC)) {
+$rid=$row["id"];
+$username=$row["username"];
+$details=$row["reminder_details"];
+$date=$row["date"];
+$time=$row["time"];
+}
+$res ='<p>';
+$res .= "$username";
+$res .= '<p>';
+$res .= "$date";
+$res .= '<p>';
+$res .= "$time";
+$res .= '<p>';
+$res .= "$details";
+}
+echo $res;
+
+?>
+
+
+
+</body>
+</html>
         </style>
 <center>
  <script type="text/javascript" src="js/IP_generalLib.js"></script>
