6d936b6c08d712a2788a5b9dc1dbee3c829c703c
Jay <jamied_uk@hotmail.com>
2019-06-22 16:27:40 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>jQuery.post demo</title>
+  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+</head>
+<body>
+
 <title>J~Net</title>
 <font size="3" color="red"> </font>
-<body style="background:#000000">
-<body bgcolor="#000000" text="#FFFFFF" link="#0000FF"> 
+
 
 <style>
 {width:100%;border:1px #444444 solid;background-color:}
@@ -16,8 +24,8 @@ A {color:#444444}
 
 
 <?php
-//include_once("../../tracker.php");
-//include_once("../../headerb.php"); 
+include_once("../../tracker.php");
+include_once("../../headerb.php");
 ?>
 <p><br><br><p><br><p>
 
@@ -35,19 +43,116 @@ error_reporting(E_ALL);
 
         <h1>Set Email Alarm</h1>
 
- <script type="text/javascript" src="js/IP_generalLib.js"></script>
-
- 
-<input type="text" name="date1" id="date1" alt="date" class="IP_calendar" title="d/m/Y">
- 
-
 
         <style type="text/css">
             body {
                 font: 14px monospace;
             }
         </style>
-    </head>
+<center>
+ <script type="text/javascript" src="js/IP_generalLib.js"></script>
+
+<form action="test.php" id="Time_And_Date"><center>
+<input type="text" name="date1" id="date1" alt="date" class="IP_calendar" title="d/m/Y">
+ 
+
+        <input type="time" id="time" name="time" value="01:30" min="0:00" max="18:02"><center>
+
+        <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="css/jquery.clockinput.css">
+        <script type="text/javascript" src="js/jquery.clockinput.js"></script>
+        <script type="text/javascript">
+            $("input[type=time]").clockInput(false);
+        </script>
+
+
+<center>
+  <input type="text" id="description" name="description" placeholder="Reminder Description" >
+  <input type="submit" value="Set" style="cursor:pointer;" >
+</form>
+<!-- the result of the search will be rendered inside this div -->
+<div id="result3"></div> <div id="result"></div> <div id="result2"></div>
+<div id="msg">Time & Date Not Set!</div>
+<script>
+// Attach a submit handler to the form
+$( "#Time_And_Date" ).submit(function( event ) {
+ 
+  // Stop form from submitting normally
+  event.preventDefault();
+ 
+  // Get some values from elements on the page:
+  var $form=$( this ),
+    date=$form.find( "input[name='date1']" ).val(),
+    time=$form.find( "input[name='time']" ).val(),
+    description=$form.find( "input[name='description']" ).val(),
+    url=$form.attr( "action" );
+ 
+  // Send the data using post
+  var posting=$.post( url, { time: time } );
+  var posting=$.post( url, { date1: date } );
+  var posting=$.post( url, { description: description } );
+  // Put the results in a div
+  posting.done(function( data ) {
+   // var content=$( data ).find( "#date1" );
+    $( "#result2" ).empty().append( time ); //content
+    $( "#result" ).empty().append( date ); //content
+    $( "#result3" ).empty().append( description ); //content
+  });
+});
+</script>
+
+
+<?php
+
+if(isset($_POST['submit'])) {
+$persinfo=preg_replace('#[^a-z0-9 :._]#i', '', $_POST['persinfo']);
+$sql="UPDATE alarm_email SET details='$persinfo', date='$persinfo1' WHERE username ='$u'";
+$query=mysqli_query($db_conx, $sql);
+session_start();
+$_SESSION["Saved"]='success';
+echo "<center>Your Future Email Alarm Has Been Updated!<br />";
+header("location: user.php?u=".$_SESSION["username"]);
+} else {
+// Update failed!
+//echo "Failed To Update!";
+}
+
+$numrows='0';
+$res='';
+
+$sql="SELECT * FROM alarm_email WHERE username ='$u' LIMIT 1";
+$user_query=mysqli_query($db_conx, $sql);
+mysqli_store_result($db_conx);
+$numrows=mysqli_num_rows($user_query);
+if($numrows < 1) {
+
+$res   ='<p> ';
+$res  .= 'No Reminders Set For Future Reminder Emails!';
+} else {
+while($row=mysqli_fetch_array($user_query, MYSQLI_ASSOC)) {
+$rid=$row["id"];
+$username=$row["username"];
+$details=$row["reminder_details"];
+$date=$row["date"];
+$time=$row["time"];
+}
+$res ='<p>';
+$res .= "$username";
+$res .= '<p>';
+$res .= "$date";
+$res .= '<p>';
+$res .= "$time";
+$res .= '<p>';
+$res .= "$details";
+}
+echo $res;
+
+?>
+
+
+
+</body>
+</html>
     <body>
 <center>
 
